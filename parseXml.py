18dc84c8e11aba42fe2dd90c5eec3154dc53ade6
favio_t_5 <favio_t_5>
2018-05-29 16:55:21 +0000

    Added mongo conn

--- parseXml.py
+++ parseXml.py
@@ -1,5 +1,23 @@
-import json, xmltodict, requests
+import json, xmltodict, requests, pymongo
 
-req = requests.post("https://www.fantasybasketballnerd.com/service/players")
+def get_db():
+    from pymongo import MongoClient
+    client = MongoClient('localhost:27017')
+    db = client.myFirstMB
+    return db
 
-print(json.dumps(xmltodict.parse(req.text), indent=4))
+def add_players(db):
+    req = requests.get("http://www.fantasybasketballnerd.com/service/players", verify=False)
+    jsonData = json.dumps(xmltodict.parse(req.text))
+    jsonConvert = json.loads(jsonData)
+    for item in jsonConvert:
+	for player in jsonConvert[item]['Player']:
+	    db.all_players.insert(player)
+
+def get_players(db):
+    return db.all_players.find_one()
+
+if __name__ == '__main__':
+    db = get_db()
+    add_players(db)
+    print get_players(db)
